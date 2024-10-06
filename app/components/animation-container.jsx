863cf97efcaf1c5651874a2bcca4e4220fec2ce2
Gauhar <asanbayg@gmail.com>
2024-10-06 14:20:49 +0500

    add images

--- app/components/animation-container.jsx
+++ app/components/animation-container.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { KangarooGif } from "./kangaroo-gif";
 
 export const AnimationContainer = ({ kangarooPopulationChange }) => {
@@ -25,10 +27,19 @@ export const AnimationContainer = ({ kangarooPopulationChange }) => {
       </div>
 
       {/* Infographics */}
-      <div className="absolute bottom-10 right-0 z-10 flex h-80 w-72 flex-wrap gap-x-8 rounded-xl border-4 border-gray-200 bg-white p-4 dark:bg-gray-800 dark:text-white">
-        <div className="h-32 w-32 border-red-50 bg-gray-500">
-          тут будет инфографика
-        </div>
+      <div className="absolute bottom-10 right-0 z-10 flex h-80 w-56 flex-wrap items-center justify-center gap-x-8 rounded-xl border-4 border-gray-200 bg-white p-4 dark:bg-gray-800 dark:text-white">
+        <Image
+          src={"/australia.jpg"}
+          width={150}
+          height={150}
+          alt="australia"
+        />
+        <Image
+          src={"/australia-change.jpg"}
+          width={150}
+          height={150}
+          alt="australia-change"
+        />
       </div>
     </>
   );
